7f73be18bbc8c7000c19d28254e8d6e4fa9c1129
Minahil Shafique <minahal.shafique@gmail.com>
2020-04-09 21:01:09 +0500

    added border to questionAnswerView

--- BootsPharmacy/BootsPharmacy/QuestionViews/QuestionTableViewCell.swift
+++ BootsPharmacy/BootsPharmacy/QuestionViews/QuestionTableViewCell.swift
@@ -26,9 +26,8 @@ class QuestionTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
+    
     func SetupCellView(_ questionText : String){
     QuestionLabel.text = questionText
     }
